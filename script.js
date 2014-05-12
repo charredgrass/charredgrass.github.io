a825b6c8a6a8746e6c295a2b03052cc7d3b35208
Maxwell Chow <maxchowla@gmail.com>
2014-05-12 16:38:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function updateDisplay(){
         document.getElementById('stuff').innerHTML = "Click on a tab to view content!"
     }
     if (cto == "stream"){
-        document.getElementById('stuff').innerHTML = "<tr><td>Date</td><td>Day</td><td>Movie</td><td>Time</td></td><tr><td>5/16</td><td>Friday</td><td>Frozen</td><td>15:00</td></tr>";
+        document.getElementById('stuff').innerHTML = "<table style="width:300px"><tr><td>Date</td><td>Day</td><td>Movie</td><td>Time</td></td><tr><td>5/16</td><td>Friday</td><td>Frozen</td><td>15:00</td></tr></table>";
         
     }
 }
