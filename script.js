81a2dba9e962f5e89ae481f4a73d861436c13ba5
Maxwell Chow <maxchowla@gmail.com>
2014-05-12 16:34:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//how do u comment
+currentTabOpen = "";
+
+
+function updateDisplay(){
+    cto = currentTabOpen;
+    if (currentTabOpen == "") {
+        document.getElementById('stuff').innerHTML = "Click on a tab to view content!"
+    }
+    if (cto == "stream"){
+        document.getElementById('stuff').innerHTML = "<tr><td>Date</td><td>Day</td><td>Movie</td><td>Time</td></td><tr><td>5/16</td><td>Friday</td><td>Frozen</td><td>15:00</td></tr>";
+        
+    }
+}
+
+function buttonStreamSchedule(){
+    if (currentTabOpen != "stream"){
+        currentTabOpen = "stream";
+    } else {
+        currentTabOpen = "";
+    }
