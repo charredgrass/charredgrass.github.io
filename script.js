9d03ecea2ed9cb62512bf83d1a7ea075b2cabe91
Maxwell Chow <maxchowla@gmail.com>
2014-05-12 16:45:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var currentTabOpen = "";
 
-updateDisplay();
+document.getElementById('stuff').innerHTML = "Click on a tab to view content!";
 
 function updateDisplay(){
     var cto = currentTabOpen;
     if (currentTabOpen == "") {
-        document.getElementById('stuff').innerHTML = "Click on a tab to view content!"
+        document.getElementById('stuff').innerHTML = "Click on a tab to view content!";
     }
     if (cto == "stream"){
         document.getElementById('stuff').innerHTML = "<table style=\"width:300px\"><tr><td>Date</td><td>Day</td><td>Movie</td><td>Time</td></td><tr><td>5/16</td><td>Friday</td><td>Frozen</td><td>15:00</td></tr></table>";
