3b713e826c22ee510e637daa93ef8656e47e3ebc
Maxwell Chow <maxchowla@gmail.com>
2014-05-14 09:15:10 -0700

    Update script.js

--- game/script.js
+++ game/script.js
@@ -1,3 +1,13 @@
 var player1 = {health:100,mana:100};
 var player2 = {health:100,mana:100};
 
+Game.init = init;
+
+function init(){
+    //stuff that happens when the canvas loads
+}
+
+function enemy(health, strength) {
+    this.health = 10;
+    this.strength = 1;
+}
