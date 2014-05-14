d037ecf8812d989a69a8a6f5b62718de3accacbd
Maxwell Chow <maxchowla@gmail.com>
2014-05-14 15:58:47 -0700

    Update script.js

--- game/script.js
+++ game/script.js
@@ -1,5 +1,5 @@
-var player1 = {health:100,mana:100};
-var player2 = {health:100,mana:100};
+var player1 = {health:100,mana:100,char:""};
+var player2 = {health:100,mana:100,char:""};
 
 Game.init = init;
 
@@ -7,7 +7,20 @@ function init(){
     //stuff that happens when the canvas loads
 }
 
-function enemy(health, strength) {
+function pickChar(player, character){
+    if (player == 1) {
+        player1.char = character;
+    } else {
+        player2.char = character;
+    }
+}
+
+
+function minion(health, strength) {
     this.health = 10;
     this.strength = 1;
 }
+
+window.onload = function(){
+    //Yes
+};
