b1de930f6c46b8c34e1cd2555d6fdf7be09cdcc7
Maxwell Chow <maxchowla@gmail.com>
2014-05-14 08:56:48 -0700

    Update script.js

--- game/script.js
+++ game/script.js
@@ -1,3 +1,11 @@
 var player1 = {health:100,mana:100};
 var player2 = {health:100,mana:100};
 
+window.onload = function() {
+	var c = document.createElement("canvas");
+	if (c.getContext && c.getContext("2d")) {
+		//Game.init();
+	} else {
+		alert("Please use a Canvas enabled browser such as Firefox, Safari, Chrome or Opera.")
+	}
+}
