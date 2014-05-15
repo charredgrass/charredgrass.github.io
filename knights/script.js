3e869d6dfea6ca6b3f5167977810c4b1504d20db
Maxwell Chow <maxchowla@gmail.com>
2014-05-15 11:25:59 -0700

    Update script.js

--- knights/script.js
+++ knights/script.js
@@ -1,9 +1,15 @@
-var PlayerName = "";
+var playerName = "";
+var kingdomName = "";
+var knights = 0;
+var peasants = 0;
+var production = 0;
+var productivity = 1;
 
 function init(){
     if (localStorage.getItem("game") != "yes"){
         console.log('No game detected.');
         playerName = prompt("What is your name?","");
+        
     } else {
         load();
     }
@@ -18,3 +24,10 @@ function load(){
 	console.log('Attempting to load game...');
 	playerName = localStorage.getItem("name"); //Don't forget to write parseInt() when loading ints
 }
+
+function setProd(){
+	production = peasants * productivity
+}
+
+
+
