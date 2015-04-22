eddf59193c9d75901140021bd476cdfb2bda322e
Maxwell Chow <maxchowla@gmail.com>
2015-04-21 21:01:59 -0700

    Chart Test

---
+++ data/script.js
@@ -0,0 +1,38 @@
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Gender'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Gender breakdown',
+            data: [
+                ['Male',   80.2],
+                ['Female',       19.8],
+                ['Other',   0.0]
+            ]
+        }]
+    });
+});
+
