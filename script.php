5fd8a1ba4a92e0170fe3bef704987d4ce760f207
Maxwell Chow <maxchowla@gmail.com>
2016-01-15 18:53:27 -0800

    fuck php

--- script.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$key = "BABFDA1BEE8FED6548F2A41EFF72CAF4";
-$url = " http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" . $key . "&steamids=76561198058896751";
-echo $url;
-
-?>
